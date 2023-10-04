a578019a2e68866845fce27c6be2da0b298070cd
Krishanu Chakraborty <krishanuc1001@users.noreply.github.com>
2023-10-05 02:03:29 +0530

    Refactor BrowserStackDriver

--- src/main/java/com/tw/driver/BrowserStackDriver.java
+++ src/main/java/com/tw/driver/BrowserStackDriver.java
@@ -1,38 +1,45 @@
 package com.tw.driver;
 
-import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 
 public class BrowserStackDriver implements IDriver {
 
     @Override
-    public AndroidDriver getDriver() throws MalformedURLException {
+    public AppiumDriver getDriver(Map<String, String> map) throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
+        map.forEach(capabilities::setCapability);
         capabilities.setCapability(MobileCapabilityType.APP, "bs://ApiDemos-debug.apk");
         capabilities.setCapability("browserstack.user", "**********");
         capabilities.setCapability("browserstack.key", "**********");
-        capabilities.setCapability("device", "Samsung Galaxy S22 Ultra");
-        capabilities.setCapability("os_version", "12");
         capabilities.setCapability("project", "BS Appium Project");
-        capabilities.setCapability("build", "BS Apium Java Android Buid");
+        capabilities.setCapability("build", "BS Appium Build");
         capabilities.setCapability("name", "BrowserStack test");
 
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Krish_Pixel_4_API_30");
         capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "io.appium.android.apis");
         capabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "io.appium.android.apis.ApiDemos");
 
-        return new AndroidDriver(new URL("https://hub.browserstack.com/wd/hub"), capabilities);
+        capabilities.asMap().forEach((k, v) -> System.out.println(k + ":" + v));
+
+        return new AppiumDriver(new URL("https://hub.browserstack.com/wd/hub"), capabilities);
     }
 
     @Override
-    public AndroidDriver getDriver(String browser) throws MalformedURLException {
+    public AppiumDriver getDriver() throws MalformedURLException {
+        return null;
+    }
+
+
+    @Override
+    public AppiumDriver getDriver(String browser, Map<String, String> map) throws MalformedURLException {
         return null;
     }
 }
